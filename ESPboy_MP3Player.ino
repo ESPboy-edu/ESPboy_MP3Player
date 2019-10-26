d2678a62a6eaae1f49b96ea6c735d848bd225120
ESPboy <ESPboy.edu@gmail.com>
2019-10-26 10:49:16 +0300

    improvements

--- ESPboy_MP3Player.ino
+++ ESPboy_MP3Player.ino
@@ -44,7 +44,7 @@ https://hackaday.io/project/164830-espboy-beyond-the-games-platform-with-wifi
 //diff vars button
 static uint_fast16_t  buttonspressed;
 static uint_fast16_t count;
-static uint16_t mp3vol, mp3state, mp3eq, mp3fileCounts, mp3currentTrack, mp3song = 0; 
+static uint16_t mp3vol, mp3state, mp3eq, mp3fileCounts, mp3currentTrack; 
 static String consolestrings[12];
 static uint16_t consolestringscolor[12];
 static unsigned long counttime;
@@ -155,25 +155,27 @@ void runButtonsCommand(){
   tone (SOUNDpin, 800, 20);
   pixels.setPixelColor(0, pixels.Color(0,0,20));
   pixels.show();  
-  if (LEFT_BUTTON && mp3song) {
+  secondarySerial.flush();
+  Serial.flush();
+  delay (50);
+  
+  if (LEFT_BUTTON && mp3currentTrack > 1) {
     mp3.prevTrack(); 
+    readMP3state();
     mp3onplay = PLAY; 
     mp3random = OFF; 
-    mp3song--; 
-    readMP3state();
     drawConsole((String)F("play prev ") + (String)mp3currentTrack, TFT_YELLOW);}
-  if (RIGHT_BUTTON && mp3song < mp3fileCounts) { 
+  if (RIGHT_BUTTON && mp3currentTrack < mp3fileCounts) { 
     mp3.nextTrack(); 
-    mp3onplay = PLAY; 
-    mp3random = OFF; 
-    mp3song++; 
     readMP3state();
+    mp3onplay = PLAY; 
+    mp3random = OFF;  
     drawConsole((String)F("play next ") + (String)mp3currentTrack,TFT_YELLOW);}
-  if (UP_BUTTON) { 
+  if (UP_BUTTON && mp3vol < 30) { 
     mp3.increaseVolume(); 
     readMP3state();
     drawConsole((String)F("volume up ") + (String)mp3vol, TFT_YELLOW);}
-  if (DOWN_BUTTON) { 
+  if (DOWN_BUTTON && mp3vol > 0) { 
     mp3.decreaseVolume();
     readMP3state(); 
     drawConsole((String)F("volume down ") + (String)mp3vol, TFT_YELLOW);}
@@ -181,12 +183,12 @@ void runButtonsCommand(){
     keypressedtime = waitkeyunpressed();
     if (keypressedtime < 1000){
       mp3.playRandomTrackFromAll();
+      readMP3state();
       mp3random = ON; 
       mp3onplay = PLAY; 
       drawConsole(F("random play"),TFT_YELLOW); 
     }
     else {
-      readMP3state();
       drawConsole((String)F("state: ") + (String)mp3state, TFT_WHITE);
       drawConsole((String)F("volume: ") + (String)mp3vol, TFT_WHITE);
       drawConsole((String)F("EQ: ") + (String)mp3eq, TFT_WHITE);
@@ -199,35 +201,38 @@ void runButtonsCommand(){
     if (keypressedtime < 1000){
        if (mp3onplay == PLAY){ 
         mp3.pause(); 
+        readMP3state();
         mp3onplay = PAUSE; 
         drawConsole(F("pause"),TFT_YELLOW);}
        else { 
         mp3.start(); 
+        readMP3state();
         mp3onplay = PLAY; 
         drawConsole(F("continue play"),TFT_YELLOW); }
     }
     else { 
       mp3.stop(); 
+      readMP3state();
       mp3onplay = STOP; 
       tone (SOUNDpin, 200, 100);
       drawConsole(F("stop play"),TFT_YELLOW);}
   }
+  
   waitkeyunpressed();
-  delay(150);
+  delay(100);
   
   if (mp3onplay == PLAY) 
-    if (!mp3random) pixels.setPixelColor(0, pixels.Color(0,20,0));
+    if (mp3random == OFF) pixels.setPixelColor(0, pixels.Color(0,20,0));
     else pixels.setPixelColor(0, pixels.Color(10,10,0));
   if (mp3onplay == PAUSE) pixels.setPixelColor(0, pixels.Color(3,0,0));
   if (mp3onplay == STOP) pixels.setPixelColor(0, pixels.Color(0,0,0));
-
   pixels.show();  
 }
 
 
 
 void batshow(){
- int btt[10], sum;
+ uint16_t btt[10], sum;
   tft.setTextColor(TFT_MAGENTA);
   tft.setCursor(108, 118);
   for (int i=0; i<10; i++)
@@ -252,6 +257,7 @@ void screenon(){
 void screenoff(){
    lcdbrightnes = 0;
    pixels.setPixelColor(0, pixels.Color(0,0,0));
+   pixels.show();
    pixels.show();  
    digitalWrite(D4, HIGH);
 }
@@ -330,6 +336,7 @@ void setup(){
     tft.print(F("MP3 module fault"));
     while(true) delay(10);
   }
+  
   mp3.reset(); 
   mp3.setVolume(MP3VOLUME);  
   mp3.setEq(MP3EQ);
@@ -342,7 +349,6 @@ void setup(){
   
 //clear TFT
   tft.fillScreen(TFT_BLACK);
-  counttime = millis();
   tft.setTextSize(1);
   tft.setTextColor(TFT_MAGENTA);
   tft.setCursor(13, 8);
@@ -354,13 +360,15 @@ void setup(){
   drawConsole(F("long A - stop"), TFT_WHITE);
   drawConsole(F("B - random"), TFT_WHITE);
   drawConsole(F("long B - stat"), TFT_WHITE);
+
+  counttime = millis();
 }
 
 
 void loop(){
   if ((millis()-counttime > 7000) && mp3onplay == PLAY) 
     if (lcdbrightnes > 1){
-      lcdbrightnes -= 100;
+      lcdbrightnes -= 50;
       if (lcdbrightnes < 0) {
        screenoff();
        lcdbrightnes = 0;
